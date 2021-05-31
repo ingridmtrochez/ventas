4579d9aff1d8cf179cf58cfa39f4c3595857ba03
Ingrid Trochez <ingridtrochez99@gmail.com>
2021-05-31 14:05:15 -0600

    Agregando frase motivadora

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Piensa,cree, sueña y atrevete"); 
+            MessageBox.Show("Piensa,cree, sueña y atrévete"); 
         }
     }
 }
